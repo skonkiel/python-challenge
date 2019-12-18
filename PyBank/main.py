38c444adf1e96154c90a7602c4974aa6e61e3555
Stacy <stacy@altmetric.com>
2019-12-17 20:42:39 -0600

    final homework changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,6 @@
 import csv
+from statistics import mean
+from collections import OrderedDict
 
 with open("./budget_data.csv", 'r') as inputfile:
     reader = csv.reader(inputfile, delimiter=',')
@@ -6,68 +8,82 @@ with open("./budget_data.csv", 'r') as inputfile:
     # Skip header in file
     csv_header = next(reader)
 
-    monthCount = 0
-    profit_loss_list = [] 
-    avg_profit = 0
-    increase = 0
-    inc_month = ""
-    decrease = 0
-    dec_month = ""
+    # Initialize dict and list for all profits/losses
+    data = {}
+    p_l = []
 
+    # Create a dict to hold month/profit+loss pairs
     for row in reader:
-        m = row[0]
-        p_l = int(row[1])
-
-        # The total number of months included in the dataset
-        monthCount += 1
-        profit_loss_list.append(p_l) # Adding that month's p/l to a list 
-
-        # The greatest increase in profits (date and amount) over the entire period
-        # Compare each new addition to the last added value; if greater than x (stored), make x
-        if increase < p_l:
-            increase = p_l
-            inc_month = m
-        # The greatest decrease in losses (date and amount) over the entire period
-        elif decrease > p_l:
-            decrease = p_l
-            dec_month = m
-        else:
-            pass
-
-
-
-        
- 
-       
-            
+        data[row[0]] = int(row[1]) 
+        p_l.append(int(row[1]))
+    
+    # The total number of months included in the dataset
+    num_months = str(len(data))
 
     # The average of the changes in "Profit/Losses" over the entire period
-    # To calculate: Get first value of profit_loss_list, subtract from last value, divide by monthCount
-    start_val = profit_loss_list[0]
-    end_val = profit_loss_list[-1]
-    net_profit = end_val - start_val
-    avg_profit = (end_val - start_val) / monthCount
+    changes = []
+    changeMonth = []
 
+    prevMonth = 0
+    month = 0
+
+    for x in data:
+        month = data[x]
+        c = month - prevMonth
+        changes.append(c)
+        changeMonth.append(x)
+        prevMonth = month
+    
+    changes.pop(0) # Remove the first value because it doesn't count towards average we want to find
+    changeMonth.pop(0)
+
+    # Get the average changes over time
+    change_avg = mean(changes)
+    change_avg = round(change_avg, 2)
+
+    # The net total amount of "Profit/Losses" over the entire period
+    net_profit = sum(p_l)
+    # print(net_profit)
+
+    # The greatest increase in profits (date and amount) over the entire period
+    # The greatest decrease in losses (date and amount) over the entire period
+    # to find greateast increase and decrease, take change and find max/min
+    increase = max(changes)
+    decrease = min(changes)
+
+    # look for index of increaase and decrease, then match each against list of months  
+    in_i = changes.index(increase)
+    in_d = changes.index(decrease)
+
+    inc_month = changeMonth[in_i] # Have to add one to each of these indices, as `changes` is one value less than `months`
+    dec_month = changeMonth[in_d]        
+
+    print(inc_month, dec_month)   
+
+    # Ready vars for printing
     decrease = str(decrease)
     increase = str(increase)
+    monthCount = str(num_months)
+    avg_profit = str(change_avg)
+    net_profit = str(net_profit)
 
-    # Print to terminal
-    print("Financial analysis")
+    # # Print to terminal
+    print("Financial Analysis")
     print("------------------")
-    print("Months: " + str(monthCount))
-    print("Net total amount of profit/loss over entire period: $" + str(net_profit))
-    print("Average profit / loss over entire period (profit/loss per month): $" + str(avg_profit))
-    print("Greatest increase: " + inc_month + ", $" + increase)
-    print("Greatest decrease: " + dec_month + ", $" + decrease)
+    print("Total Months: " + monthCount)
+    print("Total: $" + net_profit)
+    print("Average Change: $" + avg_profit)
+    print("Greatest Increase in profits: " + inc_month + " ($" + increase + ")")
+    print("Greatest Decrease in profits: " + dec_month + " ($" + decrease + ")")
 
-    # Print to text file
+    # # Print to text file
 
     with open('./analysis.txt', 'w') as writefile:
-        writefile.write("Financial analysis\n")
+        writefile.write("Financial Analysis\n")
         writefile.write("------------------\n")
-        writefile.write("Months: " + str(monthCount) + "\n")
-        writefile.write("Net total amount of profit/loss over entire period: $" + str(net_profit) + "\n")
-        writefile.write("Average profit / loss over entire period (profit/loss per month): $" + str(avg_profit) + "\n")
-        writefile.write("Greatest increase: " + inc_month + ", $" + increase + "\n")
-        writefile.write("Greatest decrease: " + dec_month + ", $" + decrease)
+        writefile.write("Total Months: " + monthCount + "\n")
+        writefile.write("Total: $" + net_profit + "\n")
+        writefile.write("Average Change: $" + avg_profit + "\n")
+        writefile.write("Greatest Increase in profits: " + inc_month + " ($" + increase + ")\n")
+        writefile.write("Greatest Decrease in profits: " + dec_month + " ($" + decrease + ")")
         writefile.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,6 +6,7 @@ with open("./election_data.csv", 'r') as inputfile:
 
     # Skip header in file
     csv_header = next(reader)
+    
     vote = {}
     pct_vote = {}
     total_votes = 0
@@ -19,6 +20,8 @@ with open("./election_data.csv", 'r') as inputfile:
         else:
             vote[candidate] = 1
     
+#     vote = {k: v for k, v in sorted(vote.items(), key=lambda item: item[1], reverse=True)} # Adapted from https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
+    
     # Get the votes for each candidate, tally total votes
     for c in vote:
         total_votes += vote[c]
@@ -31,59 +34,62 @@ with open("./election_data.csv", 'r') as inputfile:
         # Code adapted from a solution suggested here: https://stackoverflow.com/questions/5306756/how-to-print-a-percentage-value-in-python
         pct_vote[c] = str(pct)
         c = str(c) 
-    
-    # Print to terminal
-    print("Election results")
-    print("------------------") 
 
-    # Print total votes
+#     # Print to terminal
+#     print("Election results")
+#     print("------------------") 
 
-    print("Total Votes: " + str(total_votes))
-    print("------------------") 
+#     # Print total votes
+
+#     print("Total Votes: " + str(total_votes))
+#     print("------------------") 
 
     winner = ""
     comparison = 0
 
-    # Combine the dicts into one, print candidate totals
+#     # Combine the dicts into one, print candidate totals
 
-    # Figure out how to sort the printed list
-    
+    # final_tally = {}
+
     for same in vote.keys() & pct_vote.keys():
         name = same
         pct_name = pct_vote[same]
         vote_name = vote[same]
-        print(name + ": " + pct_name + " (" + str(vote_name) + ")")    
+        # print(name + ": " + pct_name + " (" + str(vote_name) + ")")    
         if vote_name > comparison:
             comparison = vote_name
             winner = name
         else:
             pass
 
-    print("------------------") 
-    print("Winner: " + winner)
-    print("------------------") 
+    # final_tally = {k: v for k, v in sorted(final_tally.items(), key=lambda item: item[1], reverse=True)} # Adapted from https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
+    
+
+#     print("------------------") 
+#     print("Winner: " + winner)
+#     print("------------------") 
  
-   # Print to text file
+#    # Print to text file
 
-    with open('./analysis.txt', 'w') as writefile:
-        writefile.write("Election results\n")
-        writefile.write("------------------\n")
-        writefile.write("Total Votes: " + str(total_votes) + "\n")
-        writefile.write("------------------\n")
+#     # with open('./analysis.txt', 'w') as writefile:
+#     #     writefile.write("Election results\n")
+#     #     writefile.write("------------------\n")
+#     #     writefile.write("Total Votes: " + str(total_votes) + "\n")
+#     #     writefile.write("------------------\n")
 
-        for same in vote.keys() & pct_vote.keys():
-            name = same
-            pct_name = pct_vote[same]
-            vote_name = vote[same]
-            writefile.write(name + ": " + pct_name + " (" + str(vote_name) + ")\n")    
-            if vote_name > comparison:
-                comparison = vote_name
-                winner = name
-            else:
-                pass
+#     #     for same in vote.keys() & pct_vote.keys():
+#     #         name = same
+#     #         pct_name = pct_vote[same]
+#     #         vote_name = vote[same]
+#     #         writefile.write(name + ": " + pct_name + " (" + str(vote_name) + ")\n")    
+#     #         if vote_name > comparison:
+#     #             comparison = vote_name
+#     #             winner = name
+#     #         else:
+#     #             pass
 
-        writefile.write("------------------\n") 
-        writefile.write("Winner: " + winner + "\n")
-        writefile.write("------------------\n") 
+#     #     writefile.write("------------------\n") 
+#     #     writefile.write("Winner: " + winner + "\n")
+#     #     writefile.write("------------------\n") 
 
-        writefile.close()
+#     #     writefile.close()
